ff07bdf1153070e70650e8a6634ea2f9872b9008
Kevin Schaaf <kevin.schaaf@lge.com>
2014-02-05 00:29:16 -0800

    Comment enyo-ilib, enyo-webos, enyo-cordova from package.js by default Enyo-DCO-1.1-Signed-Off-By: Kevin Schaaf (kevin.schaaf@lge.com)

--- source/package.js
+++ source/package.js
@@ -1,16 +1,22 @@
 enyo.depends(
 	// Cordova (PhoneGap) library
-	"$lib/enyo-cordova",
+	//"$lib/enyo-cordova",
+
 	// webOS-specific kind library
-	"$lib/enyo-webos",
+	//"$lib/enyo-webos",
+
+	// Internationalization library
+	//"$lib/enyo-ilib",
+
 	// Layout library
 	"$lib/layout",
-	// Internationalization library
-	"$lib/enyo-ilib",
+
 	// Moonstone UI library
 	"$lib/moonstone",
+
 	// Focus support library for TV applications
 	"$lib/spotlight",
+
 	// CSS/LESS style files
 	"style",
 	// Model and data definitions
