623e3bc2809ceed6ecc6b8187ade8017860a5dcc
Roy Sutton <roy.sutton@lge.com>
2014-10-07 17:46:23 -0400

    Detect errors from enyo/deploy.js

--- tools/deploy.bat
+++ tools/deploy.bat
@@ -4,8 +4,11 @@
 REM the folder this script is in (*/bootplate/tools)
 SET TOOLS=%~DP0
 
+REM application source location
+SET SRC=%TOOLS%\..
+
 REM enyo location
-SET ENYO=%TOOLS%\..\enyo
+SET ENYO=%SRC%\enyo
 
 REM deploy script location
 SET DEPLOY=%ENYO%\tools\deploy.js
@@ -14,24 +17,35 @@ REM node location
 SET NODE=node.exe
 
 REM use node to invoke deploy.js with imported parameters
-%NODE% "%DEPLOY%" %*
+ECHO %NODE% "%DEPLOY%" -T -s "%SRC%" -o "%SRC%\deploy" %*
+%NODE% "%DEPLOY%" -T -s "%SRC%" -o "%SRC%\deploy" %*
+IF ERRORLEVEL 1 GOTO err
 
 REM copy files and package if deploying to cordova webos
 :again
 if not "%1" == "" (
 
     if "%1" == "--cordova-webos" (
-		    REM copy appinfo.json and cordova*.js files
-			  SET SRC=%TOOLS%..\
-			  for %%A in ("%~dp0./..") do SET DEST=%TOOLS%..\deploy\%%~nA
-			  copy %SRC%appinfo.json %DEST%
-			  copy %SRC%cordova*.js %DEST%
+	REM copy appinfo.json and cordova*.js files
+	for %%A in ("%~dp0./..") do SET DEST=%TOOLS%..\deploy\%%~nA
+	copy %SRC%\appinfo.json %DEST%
+	IF ERRORLEVEL 1 GOTO err
+	copy %SRC%\cordova*.js %DEST%
+	IF ERRORLEVEL 1 GOTO err
 
-			  REM package it up
-				if not exist %SRC%bin mkdir %SRC%bin
-				palm-package.bat %DEST% --outdir=%SRC%bin
+	REM package it up
+	if not exist %SRC%\bin mkdir %SRC%\bin
+	palm-package.bat %DEST% --outdir=%SRC%\bin
     )
 
     shift
     goto again
 )
+
+goto done
+
+:err
+ECHO Deploy encountered errors.
+EXIT /B 1
+
+:done
